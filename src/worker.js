cabe75bffc0c42c976fd1e8ccfe40dbd4cee44f3
chamodbinancelk-afk <chamod.binance.lk@gmail.com>
2025-11-16 06:20:20 -0500

    Update worker.js

--- src/worker.js
+++ src/worker.js
@@ -1,5 +1,5 @@
 import TelegramBot from 'node-telegram-bot-api';
-import { getFbVideoInfo } from './src/services/facebook.js';
+import { getFbVideoInfo } from './src/worker/facebook.js';
 import fs from 'fs';
 
 const BOT_TOKEN = process.env.BOT_TOKEN;
